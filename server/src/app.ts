ef9ed232b291aeb075b579011e1d7d948b73709f
soyoung-choi <sydesign24@naver.com>
2022-05-20 14:00:25 +0900

    Update app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,12 +2,14 @@ import express from 'express'
 
 const app = express();
 
+const PORT = process.env.PORT || 5500
+
 app.use(express.static('build')) // 서버에서 build 폴더 사용 가능
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/build/index.html')
 })
 
-app.listen(5500, () => {
+app.listen(PORT, () => {
   console.log('server is running!')
 })
